85fe66f2eeaa06b4f7394fc8099c07a1c3813dee
cryafonic <brendonvandernest69@gmail.com>
2021-05-15 15:18:42 +0200

    adding form component

---
+++ client/src/components/formInput/FromInput.js
@@ -0,0 +1,23 @@
+function FromInput() {
+
+    function hanleChange() {
+        
+    }
+
+    function handleSubmit(){
+
+    }
+
+    return(
+        <form>
+            <input type="input" name="term"></input>
+            <lable>select media type</lable>
+            <select>
+                <option>music</option>
+                <option>movie</option>
+            </select>
+        </form>
+    );
+}
+
+export default FromInput;
