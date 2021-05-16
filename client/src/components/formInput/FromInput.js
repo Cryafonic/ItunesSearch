e3a0ae292132a225bcf1b9815826abccbf9bc8f4
Brendon <Brendonvandernest69@gmail.com>
2021-05-16 13:00:34 +0200

    form added to send data to the back-end

--- client/src/components/formInput/FromInput.js
+++ client/src/components/formInput/FromInput.js
@@ -1,20 +1,39 @@
+import { FetchParams } from "../fetchParams/FetchParams";
+
 function FromInput() {
+    const [options, setOptions] = useState({
+        input: "",
+        select: ""
+    });
 
-    function hanleChange() {
-
+    // handle the change input fields
+    function handleChange(e) {
+        const value = e.target.value;
+        setOptions({
+            ...options,
+            [e.target.name]: value
+        });
     }
-
-    function handleSubmit(){
-
-    }
-
+    
+    // post the data to the backend
+    function handleSubmit() {
+        const get = { options }
+        const options = {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(get)
+        };
+        FetchParams(options)
+    };
     return(
-        <form>
-            <input type="input" name="term"></input>
+        <form onSubmit={handleSubmit} >
+            <input type="input" name="term" onChange={handleChange} value={options.input}></input>
             <lable>select media type</lable>
-            <select>
-                <option>music</option>
-                <option>movie</option>
+            <select onChange={handleChange} >
+                <option name="select" value="music" >music</option>
+                <option name="select" value="movie" >movie</option>
             </select>
             <input type="submit" name="submit">search</input>
         </form>
